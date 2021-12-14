e7b724f241b661bdb5ae00ae1072837ff2ef4697
Johann Arouca <aroucajohann@yahoo.com>
2021-12-13 21:03:16 -0300

    13/12

--- src/components/template/SideBar.jsx
+++ src/components/template/SideBar.jsx
@@ -1,12 +1,25 @@
 import './SideBar.css'
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './Nav'
 import Logo from './Logo'
 import SocialMedias from './SocialMedias'
+import { Link } from 'react-router-dom'
 
-export default props =>
-    <aside className="side-bar-area">
-        <Logo/>
-        <Nav/>
-        <SocialMedias/>
-    </aside>
+export default function SideBar() {
+    const [sideBar, setSideBar] = useState(false);
+
+    const showSideBar = () => setSideBar(!sideBar);
+
+    return (
+        <>
+            <Link to='#' className='menu-bars'>
+                <i onClick={showSideBar} className={sideBar ? 'menu-icon fa fa-arrow-left fa-lg' : 'menu-icon fa fa-bars fa-lg'}></i>
+            </Link>
+            <aside className={sideBar ? 'side-bar-area active' : 'side-bar-area'}>
+                <Logo />
+                <Nav />
+                <SocialMedias />
+            </aside>
+        </>
+    )
+}
--- src/main/App.jsx
+++ src/main/App.jsx
@@ -5,12 +5,14 @@ import { BrowserRouter } from 'react-router-dom'
 
 import Footer from '../components/template/Footer'
 import SideBar from '../components/template/SideBar'
+import MenuButton from '../components/template/MenuButton'
 
 export default props =>
     <BrowserRouter>
         <div className="app">
            {/*  <Logo/> */}
             <SideBar/>
+            {/* <MenuButton/> */}
             <h1>TESTE</h1>
             <Footer/>
         </div>
