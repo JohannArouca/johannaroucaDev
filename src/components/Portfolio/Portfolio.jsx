3a10064989f1933da255bea6010683c259e180d1
Johann Arouca <aroucajohann@yahoo.com>
2021-12-26 20:50:23 -0300

    26/12

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -13,15 +13,61 @@ export default function Portfolio() {
 
     const handleLeftClick = (e) => {
         e.preventDefault()
-        carousel.current.scrollLeft -= carousel.current.offsetWidth
-    }
-        
-    const handleRightClick = (e) => {
-        e.preventDefault()
-        carousel.current.scrollLeft += carousel.current.offsetWidth
+        carousel.current.scrollLeft = carousel.current.scrollLeft - 400
+        /* carousel.current.scrollLeft -= carousel.current.offsetWidth */
     }
 
-    if(!data || !data.length) return null
+    const handleRightClick = (e) => {
+        e.preventDefault()
+        carousel.current.scrollLeft = carousel.current.scrollLeft + 400
+        /* carousel.current.scrollLeft += carousel.current.offsetWidth */
+    }
+
+    if (!data || !data.length) return null
+
+    return (
+        <div id="portfolio" className="portfolio">
+            <h2>{'<Portfólio>'}</h2>
+
+            <div className='carousel' ref={carousel}>
+
+                {data.map((item) => {
+                    const { id, name, image, text, technology, link } = item
+
+                    return (
+                        <div className='item' key={id}>
+                            <div className='title'>
+                                <img src={image}></img>
+                                <h3>{name}</h3>
+                            </div>
+                            <div className='paragraph'>
+                                <p>{text}</p>
+                            </div>
+                            <div className='footer'>
+                                <div className='technology'>
+                                    <img src={technology} alt="" />
+                                </div>
+                                <a href={link} target="_blank">
+                                    Ver aplicação
+                                </a>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+            <div className='buttons'>
+                <button onClick={handleLeftClick}>
+                    <i className='fa fa-angle-left fa-5x' alt="Scroll Left" />
+                </button>
+                <button onClick={handleRightClick}>
+                    <i className='fa fa-angle-right fa-5x' alt="Scroll Right" />
+                </button>
+            </div>
+        </div>
+
+    )
+
+    /*
 
     return (
         <div id="portfolio" className="portfolio">
@@ -48,5 +94,5 @@ export default function Portfolio() {
                 </button>
             </div>
         </div>
-    )
+    ) */
 }
--- src/components/SideBar/SocialMedias.jsx
+++ src/components/SideBar/SocialMedias.jsx
@@ -7,4 +7,5 @@ export default props =>
         <a href="https://github.com/JohannArouca" target="_blank" className="icon fa fa-github fa-2x"></a>
         <a href="https://www.linkedin.com/in/johann-arouca/" target="_blank" className="icon fa fa-linkedin fa-2x"></a>
         <a href="https://www.instagram.com/johannarouca/" target="_blank" className="icon fa fa-instagram fa-2x"></a>
+        <a href="mailto:aroucajohann@gmail.com" target="_blank" className="icon fa fa-envelope-o fa-2x"></a>
     </aside>
