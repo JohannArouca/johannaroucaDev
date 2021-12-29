a4439741ea47efe61122293fdfd1e1a82b5255ef
Johann Arouca <aroucajohann@yahoo.com>
2021-12-28 21:10:06 -0300

    28/12

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,21 +1,32 @@
 import './Contact.css'
 import React from 'react'
+import emailjs from 'emailjs-com';
 
-export default props =>
-    <React.Fragment>
+export default function Contact() {
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('gmailMessage', 'template_v7csa3h', e.target, 'user_dpFhTyv77pHOh9rFJNKk4')
+          .then((result) => {
+              alert('Mensagem enviada com sucesso!')
+          }, (error) => {
+            alert(`Erro: ${error.message}`)
+          });
+          e.target.reset()
+    };
+
+    return (
         <div id="contact" className="contact">
             <h2>{'<Contato>'}</h2>
 
             <div className='content'>
                 <div className='form'>
-                    <form id='contact-form'>
-                        <input type='text' name='user_name' placeholder='Nome' />
-                        <br />
-                        <input type='email' name='user_email' placeholder='Email' />
-                        <br />
-                        <textarea name='message' placeholder='Mensagem' />
-                        <br />
-                        <input type='submit' value='Enviar' />
+                    <form id='contact-form' onSubmit={sendEmail}>
+                        <input type='text' name='name' class="feedback-input" placeholder='Name' />
+                        <input type='email' name='email' class="feedback-input" placeholder='Email' />
+                        <textarea name='message' class="feedback-input" placeholder='Message' />
+                        <input type="submit" value="Enviar" />
                     </form>
                 </div>
                 <div className='icons'>
@@ -29,9 +40,10 @@ export default props =>
                     </div>
                     <div className='item'>
                         <i className="icon fa fa-instagram fa-3x"></i>
-                        <p>@johannarouca</p>
+                        <p>@JohannArouca</p>
                     </div>
                 </div>
             </div>
         </div>
-    </React.Fragment>
+    )
+}
